586f47f71e79e8b5e36f09e44b679a82f55b3f36
Diego448 <diegosg448@gmail.com>
2020-07-16 15:39:47 -0500

    Adding variables example route

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/variables')
+def variables():
+    my_number = 23
+    message = 'Hello'
+    my_decimal = 3.24
+    return render_template('variables.html', my_number=my_number, message=message, my_decimal=my_decimal)
+
+@app.route('/listing')
+def listing():
+    my_list = []
+    return ''
